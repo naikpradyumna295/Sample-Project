33d792a2a6938b7bec531f3e02f49a00f56151f5
Pradyumna-AVD <naikpradyumna295@gmail.com>
2024-07-27 14:59:46 +0530

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-APP_NAME="myapp"
-DEPLOY_DIR=~/deploy
-
-# Stop the existing application (this will vary depending on how you run your app)
-sudo systemctl stop $APP_NAME
-
-# Remove old deployment
-rm -rf $DEPLOY_DIR/$APP_NAME.war
-
-# Move new deployment
-mv $DEPLOY_DIR/myapp.war $DEPLOY_DIR/$APP_NAME.war
-
-# Start the application
-sudo systemctl start $APP_NAME
+sudo mv ~/deploy/*.war /path/to/tomcat/webapps/
+sudo systemctl restart tomcat  # or your specific service command
